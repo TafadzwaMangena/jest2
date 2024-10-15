a1b714c4f61ef8a9f329940d0d97b4aab33dbd38
Tafadzwa Mangena <mangenatafadzwamgcini@gmail.com>
2024-10-15 13:58:26 +0000

    Add game functions

---
+++ scripts/tests/game.test.js
@@ -0,0 +1,32 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const { game } = require("../game");
+
+// This code does not change for all html files I wanted to add to the DOM, might only want to change the file name.
+beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
+
+describe("game object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    });
+    test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+});
